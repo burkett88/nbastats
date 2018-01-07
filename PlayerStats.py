341ddd7fbf19a6060f15f42c48c3146ae787af01
Tyler Burkett <tyler.burkett@creditkarma.com>
2018-01-06 18:13:34 -0800

    start adding in opposing team stats

--- PlayerStats.py
+++ PlayerStats.py
@@ -2,7 +2,7 @@
 from os.path import exists
 import pandas as pd
 
-from nba_py import team, constants, player
+from nba_py import team, constants, player, game
 
 #%%
 players_path = "players.csv"
@@ -14,7 +14,7 @@ else:
 
 #%%
 # id, name, years played, team, team_id, team_code 
-person_id_list=players['PERSON_ID'].unique()[:10] #grab just 10 players
+person_id_list=players['PERSON_ID'].unique()[:15] #grab just 10 players for now
 stats=['MIN', 'FGM',
        'FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT',
        'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS',
@@ -26,6 +26,7 @@ for i in person_id_list:
     df=player.PlayerGameLogs(i).info()
     df['GAME_DATE']=pd.to_datetime(df['GAME_DATE']) 
     df=df.set_index('GAME_DATE')
+    df['OpposingTeam']=df['Game_ID'].apply(lambda x: game.BoxscoreSummary(x).game_summary()['VISITOR_TEAM_ID'])
     df_cols = [df]
     keys = [1]
     for rollingPeriod in (7,):
@@ -34,12 +35,16 @@ for i in person_id_list:
         keys.append(rollingPeriod)
     df = pd.concat(df_cols,keys=keys,names=["RollingPeriod","Stat"],axis=1)
     alllogs.append(df)
-
+#%%
 alllogs = pd.concat(alllogs)
 alllogs
+#%%
+visitingTeamid = game.BoxscoreSummary("0021601230").game_summary()['VISITOR_TEAM_ID']
+print(game.BoxscoreSummary("0021601230").game_summary())
 
 #%%
-alllogs
+PreviousGame = game.BoxscoreSummary("0021601230").last_meeting()
+TotPointsPrevGame=PreviousGame['LAST_GAME_HOME_TEAM_POINTS']+PreviousGame['LAST_GAME_VISITOR_TEAM_POINTS']
 
 #%%
 # Per ShotType/Game
